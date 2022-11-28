0a177e47e30de1ab77635668763d224e4b0728f3
Andrew <andriypomin@gmail.com>
2022-11-28 20:10:40 +0200

    First commit

---
+++ src/main/java/task1/XMLParser.java
@@ -0,0 +1,111 @@
+package task1;
+
+import org.w3c.dom.*;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ * Клас для читання XML-файлів та їх копіювання зі змінами.
+ */
+public class XMLParser {
+    private static final String FILE = "src/main/resources/task1/input.xml"; //Path of input file
+
+    //Function to read the tag
+    public static void readTag() {
+
+        //Create factory for document builder creation
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+
+        //Create file writer
+        FileWriter fw = null;
+
+        try {
+
+            DocumentBuilder db = dbf.newDocumentBuilder(); //Create document builder
+            Document document = db.parse(FILE); //Build document
+
+            NodeList nodeList = document.getElementsByTagName("person"); //Get tag list
+
+            //If we don't have such tags - finish the program
+            if (nodeList.getLength() == 0) {
+                System.out.println("Document don't have any tag \"person\".");
+                return;
+            }
+
+            //Initialize file writer with output file path
+            fw = new FileWriter("src/main/resources/task1/output.xml");
+
+            //Write open tag
+            fw.write("<persons>\n");
+
+            //Write all elements of persons
+            for (int i = 0; i < nodeList.getLength(); i++) {
+                Node node = nodeList.item(i); //Create each node
+
+                //If node is an element
+                if (node.getNodeType() == Node.ELEMENT_NODE) {
+
+                    Element element = (Element) node; //Create element from node
+
+
+                    String surname = element.getAttribute("surname"); //Get surname
+                    String name = element.getAttribute("name") + " " + surname; //Get name
+
+                    element.removeAttribute("surname"); //Remove surname from tag "person"
+
+                    element.removeAttribute("name"); //Remove name from tag "person"
+                    element.setAttribute("name", name); //Set new attribute name in tag "person"
+
+                    NamedNodeMap attributes = element.getAttributes(); //Get list of attributes
+
+                    //Write each tag in "persons"
+                    writeTag(fw, attributes);
+                }
+            }
+
+            //Write end tag
+            fw.write("</persons>\n");
+
+        } catch (ParserConfigurationException | IOException | SAXException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (fw != null) {
+
+                    //Close streams
+                    fw.flush();
+                    fw.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    //Function write tag to file copy
+    private static void writeTag(FileWriter fw, NamedNodeMap attributes) {
+
+        try {
+
+            //Write opening of tag
+            fw.write("\t<person ");
+
+            //Write all attributes of tag
+            for (int i = 0; i < attributes.getLength(); i++) {
+                fw.write(attributes.item(i) + " "); //Write each attribute
+                fw.flush(); //Clear file writer
+            }
+
+            //Write end of the tag
+            fw.write("/>\n");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/task2/FineReader.java
@@ -0,0 +1,185 @@
+package task2;
+
+import org.json.JSONObject;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+/**
+ * Клас для читання JSON-файлів з інформацією про штрафи та виводу підсумків по кожному штрафу за весь час
+ * (відсортованими за спаданням).
+ */
+public class FineReader {
+    private static final String FOLDER = "src/main/resources/task2/input/"; //Path of input files
+    private static HashMap<String, Double> fineMap; //Map with all fines with its total amount
+
+    static {
+        fineMap = new HashMap<>(); //Initialize map on the start of program
+    }
+
+    public static void readJsonObject() {
+
+        //Create array of all files we have
+        File[] folder = new File(FOLDER).listFiles();
+
+        //If folder is empty - finish the program
+        if (folder == null) {
+            System.out.println("Folder is empty");
+            return;
+        }
+
+        //Create file reader and scanner
+        FileReader fr = null;
+        Scanner scanner = null;
+
+        //Read each file in folder
+        for (File file : folder) {
+
+            try {
+
+                //Initialize file reader and scanner by file path
+                fr = new FileReader(file);
+                scanner = new Scanner(fr);
+
+                //While file is not finished to read - read all file
+                while (true) {
+
+                    //Get each JSON object
+                    String jsonString = readJsonObject(scanner);
+
+                    //If file was ended - break loop
+                    if (jsonString.equals("")) {
+                        break;
+                    }
+
+                    //Get JSON object
+                    JSONObject jsonObject = new JSONObject(jsonString);
+
+                    String type = jsonObject.getString("type"); //Get type of fine
+                    double amount = jsonObject.getDouble("fine_amount"); //Get amount of fine
+
+                    //Create temporary amount
+                    double currentAmount = 0.0;
+
+                    //If map is not empty and have such type of fine - get total amount of fine
+                    if ((!fineMap.isEmpty()) && fineMap.containsKey(type)) {
+                        currentAmount = fineMap.get(type); //Get current total amount of fine
+                    }
+
+                    //Write fine and its total amount in the map
+                    fineMap.put(type, currentAmount + amount);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+
+                    //Close streams
+                    if (scanner != null) {
+                        scanner.close();
+                    }
+                    if (fr != null) {
+                        fr.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        //Write statistics in the XML-file
+        writeStatistics();
+    }
+
+    //Function to read each JSON-object
+    private static String readJsonObject(Scanner scanner) {
+
+        //Create string builder for JSON string
+            StringBuilder jsonString = new StringBuilder();
+
+            //Read one JSON string
+            while (scanner.hasNext()) {
+
+                //Save line of JSON
+                String nextLine = scanner.nextLine();
+
+                //Skip if line is beginning of end of the file
+                if (nextLine.equals("[") || nextLine.equals("]")) {
+                    continue;
+                }
+
+                //Add line to JSON string
+                jsonString.append(nextLine);
+
+                //Break loop if JSON object was ended
+                if (nextLine.contains("},") || nextLine.equals("}")) {
+                    break;
+                }
+            }
+
+            //Return JSON string
+            return String.valueOf(jsonString);
+    }
+
+    //Function to write fines statistics in XML-file
+    private static void writeStatistics() {
+
+        //Create file writer
+        FileWriter fw = null;
+
+        try {
+
+            //Initialize file writer by the path of output file
+            fw = new FileWriter("src/main/resources/task2/statistics.xml");
+
+            //Sort map with fines by total amount
+            List<Map.Entry<String, Double>> keys = fineMap.entrySet()
+                    .stream()
+                    .sorted(Map.Entry.<String, Double>comparingByValue().reversed())
+                    .toList();
+
+            //Write open tag
+            fw.write("<fines>\n");
+
+            //Write each fine with its total amount
+            for (Map.Entry<String, Double> entry : keys) {
+                writeXMLString(fw, entry);
+            }
+
+            //Write end tag
+            fw.write("</fines>");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (fw != null) {
+
+                    //Close streams
+                    fw.flush();
+                    fw.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    //Function to write all fines with total amounts
+    private static void writeXMLString(FileWriter fw, Map.Entry<String, Double> entry) throws IOException {
+        fw.write("\t<fine>\n\t\t<type>");
+        fw.write(entry.getKey());
+        fw.write("</type>\n");
+        fw.write("\t\t<total_amount>");
+        fw.write(String.valueOf(entry.getValue()));
+        fw.write("</total_amount>\n\t</fine>\n");
+        fw.flush();
+    }
+
+}
